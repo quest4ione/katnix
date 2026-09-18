cb07593ce5e11ba3fea99f6e8823beae8be05985
quest4ione <quest4ione@gmail.com>
2026-09-18 11:30:51 +0200

    fix/refactor: fix flakemodules jank and move the import to parts.nix

--- flake/parts.nix
+++ flake/parts.nix
@@ -2,7 +2,18 @@
 {
   imports = [
     inputs.home-manager.flakeModules.home-manager
+    inputs.flake-parts.flakeModules.flakeModules
   ];
+
+  # flake-parts.flakeModules.flakeModules apperantly needs this to be defined
+  flake.flakeModules.default = {
+    assertions = [
+      {
+        assertion = false;
+        message = "katnix.flakeModules.default exists as workaround. Do not use it.";
+      }
+    ];
+  };
 
   systems = [
     "x86_64-linux"
