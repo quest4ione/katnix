e9b83035ca2f6388e93d91e0d5be3d67e6c7ccb7
quest4ione <quest4ione@gmail.com>
2026-09-18 11:26:36 +0200

    fix: home-manager flakemodule was not imported

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     import-tree.url = "github:denful/import-tree";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
     nix-wrapper-modules.inputs.nixpkgs.follows = "nixpkgs";
   };
--- flake/parts.nix
+++ flake/parts.nix
@@ -1,4 +1,9 @@
+{ inputs, ... }:
 {
+  imports = [
+    inputs.home-manager.flakeModules.home-manager
+  ];
+
   systems = [
     "x86_64-linux"
   ];
